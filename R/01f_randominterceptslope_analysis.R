af4debfd340becc3c02e3b81e6c072493a7b1dec
Mike Bader <michaeldmbader@gmail.com>
2020-06-22 00:20:04 -0400

    Updates random intercept and slope

--- R/01f_randominterceptslope_analysis.R
+++ R/01f_randominterceptslope_analysis.R
@@ -3,7 +3,7 @@
 ##              and random slopes for 150 largest metros
 ## Author: Michael Bader
 
-## Show that slopes do vary across metro areas in Zillow data
+## SHOW THAT SLOPES DO VARY ACROSS METRO AREAS IN ZILLOW DATA
 source('R/01d_randomintercepts_analysis.R')
 betas <- by(zillow.long,zillow.long$RegionID,function(d) lm(lnvalue_ti~month,data=d))
 slopes <- t(sapply(betas,coef))[,"month"]
@@ -16,6 +16,7 @@ source('_functions.R')
 library(MASS)
 library(lme4)
 library(ggplot2)
+library(cowplot)
 
 ## GATHER THE DATA
 ## The data are the same as those used in the random intercept model
@@ -35,19 +36,18 @@ g.samp
 ## ANALYZE THE DATA
 m.ana <- lmer(lnvalue_ti ~ month + (1 + month | RegionID),data=zillow.long)
 summary(m.ana)
-m.ana.fe <- fixef(m.ana)
 m.ana.re <- ranef(m.ana)$RegionID
 
 test <- m.ana.re[abs(m.ana.re[,1])<.02,]
 zillow.long[zillow.long$RegionID%in%rownames(test),"RegionName"]
 
 ## INTERPRET THE DATA
-g.pred <- geom_abline(intercept=m.ana.fe[1],slope=m.ana.fe[2],
+g.pred <- geom_abline(intercept=m.ana@beta[1],slope=m.ana@beta[2],
                       col="orange",size=1.2)
 g.ana <- ggplot(data=zillow.long,aes(x=month,y=lnvalue_ti,group=RegionID)) +
             geom_line() +
             g.pred +
-            scale_x_continuous(breaks=seq(0,12,1),labels=rep(month.abb,2)[5:17])
+            scale_x_continuous(breaks=seq(0,24,1),labels=rep(month.abb,3)[3:27])
 g.ana
 
 ## Record predicted values and total error
@@ -55,21 +55,61 @@ zillow.long$lnvalue_ti_hat <- predict(m.ana,re.form=NA)
 zillow.long$e_tot <- zillow.long$lnvalue_ti - zillow.long$lnvalue_ti_hat ## Total error
 
 ## Record stochastic components of the model
-zillow.long$r_0i <- predict(m.ana,re.form=~(1|RegionID),random.only=T)
-zillow.long$r_1i <- rep(m.ana.re[,2],each=13)
-zillow.long$r_1iXt <- predict(m.ana,re.form=~(0+month|RegionID),random.only=T)
+m.ana.re <- ranef(m.ana)[["RegionID"]]
+zillow.long$r_0i <- rep(m.ana.re[,1], each=25)
+zillow.long$r_1i <- rep(m.ana.re[,2], each=25)
+zillow.long$r_1iXt <- zillow.long$r_1i * zillow.long$month
 zillow.long$e_ti <- (zillow.long$e_tot - zillow.long$r_0i - zillow.long$r_1iXt)
+
+## Examine distribution of residuals
+g.r0i <- qplot(m.ana.re[,1], bins=10)        ## Distribution of r_0i
+g.r1i <- qplot(m.ana.re[,2], bins=10)        ## Distribution of r_1i
+g.eti <- qplot(zillow.long$e_ti, bins=1000)  ## Distribution of e_ti
+(g.err <- plot_grid(g.r0i, g.r1i, g.eti, ncol=1))
 
 ## EXAMPLE METRO-SPECIFIC TRENDS
 ex.metros <- c(394640,394974,395012)
 zillow.ex.metros <- zillow.long[zillow.long$RegionID%in%ex.metros,]
 g.ex <- ggplot(zillow.ex.metros,
        aes(x=month,y=lnvalue_ti,color=RegionName)) +
-    geom_line(size=.5,linetype=2) +
-    geom_smooth(method="lm",se=FALSE,size=.75) +
+    geom_line(size=.75,linetype=2) +
+    geom_smooth(method="lm",se=FALSE,size=.5) +
     g.pred
 g.ex
 ggsave("images/0105_randominterceptsslopes_example.png",
        plot=g.ex,height=2.5,width=4,units="in")
 
-zillow.ex.metros[zillow.ex.metros$month==0,c("RegionName","r_0i","r_1i")]
+(ex.tbl <- zillow.ex.metros[zillow.ex.metros$month==0,c("RegionName","r_0i","r_1i")])
+
+## Ignore Below (used for writing values to my lecture notes)
+m.ana.fe <- fixef(m.ana)
+f <- file("lecture/_0105-analysis-estimates.tex")
+corrcoef <- attr(VarCorr(m.ana)$RegionID, "correlation")
+writeLines(c(
+    paste0("\\newcommand{\\intercept}{",round(m.ana.fe[1],3),"}"),
+    paste0("\\newcommand{\\interceptexp}{", round(exp(m.ana.fe[1])), "}"),
+    paste0("\\newcommand{\\slope}{",round(m.ana.fe[2], 5),"}"),
+    paste0("\\newcommand{\\slopepct}{",round(m.ana.fe[2]*100, 3),"}"),
+    paste0("\\newcommand{\\slopeexp}{", round(exp(m.ana.fe[2]), 2),"}"),
+    paste0("\\newcommand{\\tauint}{", round(vcov(m.ana)[1,1], 4), "}"),
+    paste0("\\newcommand{\\sqrttauint}{", round(sqrt(vcov(m.ana)[1,1]), 4), "}"),
+    paste0("\\newcommand{\\tauintpct}{", round(sqrt(vcov(m.ana)[1,1])*100, 2), "}"),
+    paste0("\\newcommand{\\tauslp}{", round(vcov(m.ana)[2,2], 11), "}"),
+    paste0("\\newcommand{\\sqrttauslp}{", round(sqrt(vcov(m.ana)[2,2]), 4), "}"),
+    paste0("\\newcommand{\\tauslppct}{", round(sqrt(vcov(m.ana)[2,2])*100, 2), "}"),
+    paste0("\\newcommand{\\taucor}{", round(vcov(m.ana)[1,2], 6), "}"),
+    paste0("\\newcommand{\\sqrttaucor}{", round(corrcoef[1,2], 3), "}"),
+    paste0("\\newcommand{\\sigmaparsq}{", round(attr(VarCorr(m.ana), "sc")^2, 6), "}"),
+    paste0("\\newcommand{\\sigmapar}{", round(attr(VarCorr(m.ana), "sc"), 4), "}"),
+    
+    paste0("\\newcommand{\\grarnot}{", round(ex.tbl[1,2], 3), "}"),
+    paste0("\\newcommand{\\grarone}{", round(ex.tbl[1,3], 5), "}"),
+    paste0("\\newcommand{\\phlrnot}{", round(ex.tbl[2,2], 3), "}"),
+    paste0("\\newcommand{\\phlrone}{", round(ex.tbl[2,3], 5), "}"),
+    paste0("\\newcommand{\\ralrnot}{", round(ex.tbl[3,2], 3), "}"),
+    paste0("\\newcommand{\\ralrone}{", round(ex.tbl[3,3], 5), "}")
+    
+    
+),f)
+close(f)
+
--- R/01f_randominterceptslope_simulation.R
+++ R/01f_randominterceptslope_simulation.R
@@ -12,31 +12,31 @@ library(MASS)
 
 #### SIMULATION EXAMPLE: RANDOM INTERCEPTS & SLOPES
 ## PLAN THE POPULATION
-month <- c(0:12)
-N <- 150
-T <- length(month)
-t <- rep(month,N)
-i <- rep(c(1:N),each=T)
+month <- c(0:23)
+N  <- 150
+Nt <- length(month)
+t  <- rep(month,N)
+i  <- rep(c(1:N),each=Nt)
 
 gamma_00 <- log(117)
 gamma_10 <- 0.005
 sigma_ti <- 0.002
-tau_00 <- 0.10^2  ## Note that t_00, tau_11, and t_01 represent
-tau_11 <- .005^2 ## *variances/covariances* not standard deviations
-tau_01 <- 0
-varcov <- matrix(c(tau_00,tau_01,tau_01,tau_11),nrow=2)
+tau_00   <- 0.10^2  ## Note that t_00, tau_11, and t_01 represent
+tau_11   <- 0.005^2  ## *variances/covariances* not standard deviations
+tau_01   <- 0
+varcov   <- matrix(c(tau_00,tau_01,tau_01,tau_11),nrow=2)
 
 ## CONJURE THE POPULATION
 ## Set metropolitan area characteristics
-metros <- mvrnorm(N,c(0,0),varcov)
-var(metros)
-beta_0i <- gamma_00 + metros[,1]
-beta_1i <- gamma_10 + metros[,2]
+tau <- mvrnorm(N,c(0,0),varcov)
+var(tau)
+beta_0i <- gamma_00 + tau[,1]
+beta_1i <- gamma_10 + tau[,2]
 
 ## Create individual change trajectories
-lnvalue_ti <- rep(beta_0i,each=T) + rep(beta_1i,each=T)*t + rnorm(N*T,0,sigma_ti)
+lnvalue_ti <- rep(beta_0i,each=Nt) + rep(beta_1i,each=Nt)*t + rnorm(N*Nt,0,sigma_ti)
 d.sim <- data.frame(i,t,lnvalue_ti)
-head(d.sim,18)
+head(d.sim,30)
 
 ## DESCRIBE THE DATA
 g.sim <- ggplot(d.sim,aes(x=t,y=lnvalue_ti,group=i)) + geom_line()
@@ -53,13 +53,14 @@ m.sim <- lmer(lnvalue_ti ~ t + (1 + t|i),data=d.sim)
 summary(m.sim)
 m.sim.fe <- fixef(m.sim)
 m.sim.re <- ranef(m.sim)$i
-var(m.sim.re)  ## Compare to `varcov` above
+var(m.sim.re)
+cbind(var(m.sim.re), var(tau))  ## Compare to `varcov` above
 
 g.pred <- geom_abline(intercept=m.sim.fe[1],slope=m.sim.fe[2],
                       color="orange",size=1.5)
 g.sim <- ggplot(d.sim,aes(x=t,y=lnvalue_ti,group=i)) +
     geom_line() + g.pred +
-    scale_x_continuous(breaks=seq(0,12,1),labels=rep(month.abb,2)[5:17])
+    scale_x_continuous(breaks=seq(0,23,1),labels=rep(month.abb,3)[3:26])
 g.sim
 
 ## Record predicted values and total error
@@ -67,23 +68,23 @@ d.sim$lnvalue_ti_hat <- predict(m.sim,re.form=NA)
 d.sim$e_tot <- d.sim$lnvalue_ti - d.sim$lnvalue_ti_hat ## Total error
 
 ## Record stochastic components of the model
-d.sim$r_0i <- predict(m.sim,re.form=~(1|i),random.only=T)
+d.sim$r_0i <- predict(m.sim,re.form=~(1|i),random.only=TRUE)
 d.sim$r_1i <- ranef(m.sim)$i[,2][d.sim$i]
-d.sim$r_1iXt <- predict(m.sim,re.form=~(0+t|i),random.only=T)
+d.sim$r_1iXt <- predict(m.sim,re.form=~(0+t|i),random.only=TRUE)
 d.sim$e_ti <- (d.sim$e_tot - d.sim$r_0i - d.sim$r_1iXt)
 
 
-i_ex <- c(26,57,105)
+i_ex <- sample(unique(d.sim$i), 5)
 d.sim$i <- factor(d.sim$i)
 g.ex <- ggplot(d.sim[d.sim$i%in%i_ex,],aes(x=t,y=lnvalue_ti,group=i,linetype=i)) +
     g.pred + geom_line() +
-    scale_linetype_manual(values=c(1,2,3),labels=c("a","b","c"))
+    scale_linetype_manual(values=1:5,labels=letters[1:5])
 g.ex
 
-library(xtable)
+library(knitr)
 d.ex <- m.sim.re[i_ex,]
-rownames(d.ex) <- c("a","b","c")
+rownames(d.ex) <- letters[1:5]
 names(d.ex) <- c("beta\\_0","beta\\_1")
-xtable(d.ex,digits=4)
+kable(d.ex,digits=4, format="pandoc")
 
 
