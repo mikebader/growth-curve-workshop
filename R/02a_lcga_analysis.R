dc8a5235618f07109becdc7240d0434ac0af97db
mikebader <michaeldmbader@gmail.com>
2021-06-21 18:28:02 -0400

    Update LCGA to include gridssearch

--- R/02a_lcga_analysis.R
+++ R/02a_lcga_analysis.R
@@ -9,25 +9,11 @@ library(tidyverse)
 library(lcmm)
 
 load("../data/dc-nhw-population.Rdata")
-# trts <- read_csv("../data/nhgis0084_csv/nhgis0084_ts_geog2010_tract.csv") 
-# 
-# dcpop <- filter(trts, STATEA=="11") %>%
-#     group_by(TRACTA) %>%
-#     mutate(
-#         year = DATAYEAR,
-#         t = (year - 1990) / 10,
-#         nhd = TRACTA,
-#         i = cur_group_id(),
-#         totpop = CL8AA,
-#         pnhw = 100 * CY8AA / totpop
-#     ) %>%
-#     ungroup() %>%
-#     arrange(i, t) %>%
-#     select(nhd, year, i, t, pnhw)
 
+## DESCRIBE THE DATA
 p_base <- ggplot(dcpop, aes(x = t, y = pnhw, group = i)) +
     labs(
-        title = "Percent non-Hispanic white in DC neighborhoods 1990-2000",
+        title = "Percent non-Hispanic white in DC neighborhoods 1990-2010",
         y = "Percent non-Hispanic white",
         x = "Year") +
     ylim(0, 100) +
@@ -35,7 +21,8 @@ p_base <- ggplot(dcpop, aes(x = t, y = pnhw, group = i)) +
 p_ana <- p_base + geom_line(size=0.1) 
 p_ana
 
-fitnames <- c("G", "loglik", "conv", "AIC", "BIC", "entropy", "%class")
+## ANALYZE THE DATA USING LCGA
+fitnames <- c("G", "npm", "loglik", "conv", "AIC", "BIC", "entropy", "%class")
 dcpop <- as.data.frame(dcpop)
 m1 <- hlme(pnhw ~ t, subject = "i", data = dcpop)
 m2 <- hlme(pnhw ~ t, subject = "i", mixture = ~t, data = dcpop, ng = 2, B = random(m1))
@@ -49,14 +36,31 @@ summarytable(m2, m3, m4,
 m5 <- hlme(pnhw ~ t, subject = "i", mixture = ~t, data = dcpop, ng = 5, B = random(m1))
 summarytable(m2, m3, m4, m5,
              which = fitnames)
+m5g <- gridsearch(hlme(pnhw ~ t, subject = "i", mixture = ~t, data = dcpop, ng = 5), 
+    rep=100, maxiter=30, minit=m1)
+summarytable(m2, m3, m4, m5, m5g,
+             which = fitnames)
+
+m6g <- gridsearch(hlme(pnhw ~ t, subject = "i", mixture = ~t, data = dcpop, ng = 6), 
+                  rep=100, maxiter=30, minit=m1)
+summarytable(m2, m3, m4, m5, m5g, m6g,
+             which = fitnames)
+
+m7g <- gridsearch(hlme(pnhw ~ t, subject = "i", mixture = ~t, data = dcpop, ng = 7), 
+                         rep=100, maxiter=30, minit=m1)
+summarytable(m2, m3, m4, m5, m5g, m6g, m7g,
+             which = fitnames)
+
+
 
 ## Summarize the best-fitting model
-summary(m4)
+summary(m6g)
 
 ## Plot predicted values of classes
-m4p <- predictY(m4, data.frame(t=0:2), var.time = 't')
-pr <- as_tibble(m4p$pred) %>% 
-    mutate(t=as.vector(m4p$times[,1])) %>%
+m <- m6g
+mp <- predictY(m, data.frame(t=0:2), var.time = 't')
+pr <- as_tibble(mp$pred) %>% 
+    mutate(t=as.vector(mp$times[,1])) %>%
     pivot_longer(cols=starts_with("Ypred"), names_to="class", values_to="pwht",
                  names_pattern = ".+(\\d)$")
 p_pr <- p_ana + 
@@ -64,14 +68,14 @@ p_pr <- p_ana +
 p_pr
 
 ## Calculate the probabilities of neighborhoods being in each class
-denom <- sum(sapply(c(m4$best[1:3], 0), exp))
-pi_ic <- sapply(c(m4$best[1:3], 0), function(x) exp(x)/denom)
-names(pi_ic) <- paste0("pi_i(c=", 1:4, ")")
+denom <- sum(sapply(c(m$best[1:(m$ng-1)], 0), exp))
+pi_ic <- sapply(c(m$best[1:(m$ng-1)], 0), function(x) exp(x)/denom)
+names(pi_ic) <- paste0("pi_i(c=", 1:m$ng, ")")
 pi_ic
 
 ## Add information about the probabilities of class membership to data
 d_res<- dcpop %>%
-    left_join(mutate(m4$pprob, across(where(is.numeric), round, 4), by="i")) %>%
+    left_join(mutate(m$pprob, across(where(is.numeric), round, 4), by="i")) %>%
     mutate(
         class = factor(as.character(class))
     )
